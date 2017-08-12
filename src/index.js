ca03398b9af01ed8bc0ab9054d922b9b51969298
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-08-12 23:29:17 +0700

    feat(label): Add month

--- src/index.js
+++ src/index.js
@@ -16,14 +16,38 @@ const GCGraph = id => {
 }
 
 const drawGCGraph = ({ id, boxSize, boxes, limit, padding }) => {
+  // Canvas
   const draw = SVG(id).size('100%', '100%')
 
-  let index = 0
+  // Months
+  const monthX = 0
+  const monthY = 10
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const currentMonth = new Date().getMonth()
+  console.log(currentMonth)
+  const slideMonths = months.slice(currentMonth, 12).concat(months.slice(0, currentMonth))
 
+  let monthIndex = 0
+  slideMonths.map(month => {
+    let text = draw.text(slideMonths[monthIndex])
+    text
+      .font({
+        family: 'Helvetica'
+      })
+      .move(monthX + monthIndex * 40, monthY)
+
+    // next
+    monthIndex++
+  })
+
+  // Box
+  const boxX = 0
+  const boxY = 40
+  let index = 0
   boxes.map(box => {
     // move
-    const i = (boxSize + padding) * (index % limit)
-    const j = (boxSize + padding) * Math.floor(index / limit)
+    const i = boxX + (boxSize + padding) * (index % limit)
+    const j = boxY + (boxSize + padding) * Math.floor(index / limit)
 
     // shape
     draw.rect(boxSize, boxSize).move(i, j).fill(box.color)
