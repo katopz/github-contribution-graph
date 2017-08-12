a5216f3a0d7691702fe1f4844838ce159ce2d7bd
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-08-12 22:25:44 +0700

    feat(svg): Working svg.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-function component () {
+import SVG from 'svg.js'
+
+const gcg = {
+  id: 'gcg',
+  limit: 3,
+  padding: 2
+}
+
+const GCGraph = () => {
   var element = document.createElement('div')
   element.innerHTML = 'Hi!!'
+  element.id = gcg.id
 
   return element
 }
 
-document.body.appendChild(component())
+const drawGCGraph = ({ boxSize, boxes, limit, padding }) => {
+  const draw = SVG(gcg.id).size('100%', '100%')
+
+  let index = 0
+
+  boxes.map(box => {
+    // move
+    const i = boxSize * (index % limit) + padding
+    const j = boxSize * Math.floor(index / limit) + padding
+
+    // shape
+    draw.rect(boxSize, boxSize).move(i, j).fill(box.color)
+
+    // next
+    index++
+  })
+}
+
+document.body.appendChild(GCGraph())
+drawGCGraph({
+  limit: gcg.limit,
+  padding: gcg.padding,
+  boxSize: 16,
+  boxes: [{ color: 'red' }, { color: 'green' }, { color: 'blue' }, { color: 'red' }, { color: 'green' }, { color: 'blue' }]
+})
