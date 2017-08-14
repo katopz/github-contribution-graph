03cd115292efd38c0d64a095b47e861e20e776bd
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-08-14 14:00:14 +0700

    fix(helper): Correct total days

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
 const daysInMonth = (month, year) => new Date(year, month, 0).getDate()
+const isLeapYear = year => (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+const totalDaysInYear = year => (isLeapYear(year) ? 366 : 365)
 
-export { daysInMonth }
+export { daysInMonth, isLeapYear, totalDaysInYear }
--- src/seeder.js
+++ src/seeder.js
@@ -1,13 +1,15 @@
 // This is mock data for testing purpose
-const getBoxes = currentYear => {
+import { totalDaysInYear } from './helper'
+
+const getBoxes = year => {
   const contributeOrNot = (count, at) => (count === 0 ? 'No contributions' : `${count} contributions`)
-  const MAX_DAY = 365
-  let boxes = []
-  for (let i = 0; i < MAX_DAY; i++) {
+  const totalDays = totalDaysInYear(year)
+  const boxes = []
+  for (let i = 0; i <= totalDays; i++) {
     boxes.push({
       id: `b${i}`,
       color: ['#ecf0f1', '#2ecc71', '#f1c40f', '#f39c12', '#e67e22', '#d35400', '#e74c3c'][Math.floor(Math.random() * 7)],
-      data: `${contributeOrNot(Math.floor(10 * Math.random()))} on ${new Date(currentYear, 0, i).toDateString()}`
+      data: `${contributeOrNot(Math.floor(10 * Math.random()))} on ${new Date(year, 0, i).toDateString()}`
     })
   }
 
